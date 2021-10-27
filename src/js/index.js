f9315aa9208093f65968c0a03e8820eed3a0a689
unknown <pedromarcelino322@gmail.com>
2021-10-27 21:14:51 +0200

    Portfolio

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import initTilt from "./tilt";
 import initSr from "./sr";
-import { addResume } from "./utils";
-import resume from "../assets/resume.pdf";
+
 
 export default function initApp() {
   initSr();
